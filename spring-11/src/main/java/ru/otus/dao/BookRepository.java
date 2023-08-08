e9c5eccc3480a8180af3809dfbf5b492f99ac0d5
ignat934@gmail.com <ignat934@gmail.com>
2023-08-08 18:59:24 +0300

    v2

--- spring-11/src/main/java/ru/otus/dao/BookRepository.java
+++ spring-11/src/main/java/ru/otus/dao/BookRepository.java
@@ -1,10 +1,15 @@
 package ru.otus.dao;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.domain.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
   List<Book> findAllByName(String name);
+
+  @EntityGraph(type = EntityGraphType.FETCH, attributePaths = {"author", "genre"})
+  List<Book> findAll();
 }
